b10bfe06b09122bd5932ce8048a9cda41f9c32c2
Lukasz Mederak <lukasz.mederak@gmail.com>
2022-09-06 16:30:02 +0200

    Add diet dinner delete modal

--- src/pages/dashboard/diets/editDiet/components/views/manyDaysView/dinner/Dinner.tsx
+++ src/pages/dashboard/diets/editDiet/components/views/manyDaysView/dinner/Dinner.tsx
@@ -9,47 +9,51 @@ import * as Styled from "./Dinner.styles";
 
 //components
 import Image from "components/form/images/image/Image";
+import DeleteDinnerModalContent from "./deleteModal/DeleteModalContent";
 
 //icons
 import { FaEdit, FaTrash, FaInfoCircle } from "icons/icons";
+import Modal from "components/modal/Modal";
 
 interface IDietDinner {
   dietDinner: IDietDinnerQueryData;
 }
 
 const Dinner = ({ dietDinner }: IDietDinner) => {
+  const [openDeleteDinnerModal, setOpenDeleteDinnerModal] = useState(false);
   const [openDinnerOptions, setOpenDinnerOptions] = useState(false);
   const { image } = dietDinner.dinnerPortion.dinner;
 
   return (
-    <Styled.DietDinnerWrapper
-      onMouseEnter={() => setOpenDinnerOptions(true)}
-      onMouseLeave={() => setOpenDinnerOptions(false)}
-    >
-      <Styled.DietDinner>
-        {image && (
-          <div>
-            <Image roundedDataGrid={true} imageId={image} />
-          </div>
-        )}
-        <h4>{dietDinner.dinnerPortion.dinner.name}</h4>
-      </Styled.DietDinner>
-      <Styled.DietDinnerTotalWrapper>
-        <p>
-          B: <b>{dietDinner.dinnerPortion.total.protein.gram}</b>
-        </p>
-        <p>
-          T: <b>{dietDinner.dinnerPortion.total.fat.gram}</b>
-        </p>
-        <p>
-          W: <b>{dietDinner.dinnerPortion.total.carbohydrates.gram}</b>
-        </p>
-        <p>
-          kcal: <b>{dietDinner.dinnerPortion.total.kcal}</b>
-        </p>
-      </Styled.DietDinnerTotalWrapper>
+    <>
+      <Styled.DietDinnerWrapper
+        onMouseEnter={() => setOpenDinnerOptions(true)}
+        onMouseLeave={() => setOpenDinnerOptions(false)}
+      >
+        <Styled.DietDinner>
+          {image && (
+            <div>
+              <Image roundedDataGrid={true} imageId={image} />
+            </div>
+          )}
+          <h4>{dietDinner.dinnerPortion.dinner.name}</h4>
+        </Styled.DietDinner>
+        <Styled.DietDinnerTotalWrapper>
+          <p>
+            B: <b>{dietDinner.dinnerPortion.total.protein.gram}</b>
+          </p>
+          <p>
+            T: <b>{dietDinner.dinnerPortion.total.fat.gram}</b>
+          </p>
+          <p>
+            W: <b>{dietDinner.dinnerPortion.total.carbohydrates.gram}</b>
+          </p>
+          <p>
+            kcal: <b>{dietDinner.dinnerPortion.total.kcal}</b>
+          </p>
+        </Styled.DietDinnerTotalWrapper>
 
-      {/* <div>
+        {/* <div>
         produkty:{" "}
         {dietDinner.dinnerPortion.dinnerProducts.map((dinnerPortionProduct) => (
           <div key={dinnerPortionProduct.dinnerProductId}>
@@ -58,26 +62,39 @@ const Dinner = ({ dietDinner }: IDietDinner) => {
           </div>
         ))}{" "}
       </div> */}
-      <AnimatePresence>
-        {openDinnerOptions && (
-          <Styled.DietDinnerOptionsWrapper
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Styled.OptionWrapper optionType="info">
-              <FaInfoCircle />
-            </Styled.OptionWrapper>
-            <Styled.OptionWrapper optionType="edit">
-              <FaEdit />
-            </Styled.OptionWrapper>
-            <Styled.OptionWrapper optionType="delete">
-              <FaTrash />
-            </Styled.OptionWrapper>
-          </Styled.DietDinnerOptionsWrapper>
-        )}
-      </AnimatePresence>
-    </Styled.DietDinnerWrapper>
+        <AnimatePresence>
+          {openDinnerOptions && (
+            <Styled.DietDinnerOptionsWrapper
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Styled.OptionWrapper optionType="info">
+                <FaInfoCircle />
+              </Styled.OptionWrapper>
+              <Styled.OptionWrapper optionType="edit">
+                <FaEdit />
+              </Styled.OptionWrapper>
+              <Styled.OptionWrapper
+                optionType="delete"
+                onClick={() => setOpenDeleteDinnerModal(true)}
+              >
+                <FaTrash />
+              </Styled.OptionWrapper>
+            </Styled.DietDinnerOptionsWrapper>
+          )}
+        </AnimatePresence>
+      </Styled.DietDinnerWrapper>
+      <Modal
+        open={openDeleteDinnerModal}
+        onClose={() => setOpenDeleteDinnerModal(false)}
+      >
+        <DeleteDinnerModalContent
+          dietDinner={dietDinner}
+          closeModal={() => setOpenDeleteDinnerModal(false)}
+        />
+      </Modal>
+    </>
   );
 };
 
---
+++ src/pages/dashboard/diets/editDiet/components/views/manyDaysView/dinner/deleteModal/DeleteModalContent.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "utils/api";
+import { mutate } from "swr";
+import { IDietDinnerQueryData } from "interfaces/diet/dietQuery.interfaces";
+
+//styles
+import * as Styled from "./DeleteModalContent.styles";
+import Button from "components/form/button/Button";
+
+//components
+import Heading from "components/heading/Heading";
+
+//icons
+import { FaUtensils } from "react-icons/fa";
+
+//assets
+import DeleteImg from "assets/delete.svg";
+
+const DeleteModalContent = ({
+  dietDinner,
+  closeModal,
+}: {
+  dietDinner: IDietDinnerQueryData;
+  closeModal: () => void;
+}) => {
+  const deleteDietDinner = async () => {
+    try {
+      await axios.delete(`/api/v1/dietDinners/${dietDinner._id}`, {
+        withCredentials: true,
+      });
+
+      console.log("usunięto posiłek z diety");
+      closeModal();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Styled.ModalContentWrapper>
+      <Heading
+        icon={<FaUtensils />}
+        title="Usuń posiłek"
+        // description={t("dinner.form.products.modal.description")}
+      />
+      <Styled.ContentWrapper>
+        <img src={DeleteImg} />
+        <h2>
+          Czy napewno chcesz usunąć{" "}
+          <b>{dietDinner.dinnerPortion.dinner.name}</b> ?
+        </h2>
+
+        <Button type="button" onClick={deleteDietDinner as () => void}>
+          usuń posiłek
+        </Button>
+      </Styled.ContentWrapper>
+    </Styled.ModalContentWrapper>
+  );
+};
+
+export default DeleteModalContent;
